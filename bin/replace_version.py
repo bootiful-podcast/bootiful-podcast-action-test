e72d61d27c36e8da1fe3b763efc742f33645c55a
Josh Long <josh@joshlong.com>
2020-11-18 15:54:25 -0800

    updating version to 6

--- bin/replace_version.py
+++ bin/replace_version.py
@@ -8,9 +8,9 @@ if __name__ == '__main__' :
     print (f'the version is {version}.')
     yml = os.path.abspath (  f'{this_script_dir}/../.github/workflows/deploy.yml' )
     contents = open(yml , 'r').read()
-    new_content  = contents.replace('v1', 'v%s' % version)
+    new_content  = contents.replace('v%s' %  (int(version)  -1) , 'v%s' % version)
     with open ( yml, 'w') as fp :
         fp.write (new_content)
-        fp.write ( int(version) * ' ')
+        # fp.write ( int(version) * ' ')
 
 
