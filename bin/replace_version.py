0545d39890c5e555ef17c7971ba6c3b3539a8cd7
Josh Long <josh@joshlong.com>
2020-11-18 17:17:43 -0800

    updating version to

--- bin/replace_version.py
+++ bin/replace_version.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
-import os,sys, re
+import os
+import re
+import sys
+import urllib.request
 
-if __name__ == '__main__' :
-    this_script_dir   = os.path.dirname(  sys.argv[ 0] )
-    version = sys.argv[1]
-    print (f'the version is {version}.')
-    yml = os.path.abspath (  f'{this_script_dir}/../.github/workflows/deploy.yml' )
-    contents = open(yml , 'r').read()
-    new_content  = contents.replace('v%s' %  (int(version)  -1) , 'v%s' % version)
-    with open ( yml, 'w') as fp :
-        fp.write (new_content)
+if __name__ == '__main__':
+
+    version = 1
+    try:
+        with urllib.request.urlopen(
+                'https://raw.githubusercontent.com/bootiful-podcast/bootiful-podcast-action/main/version') as f:
+            version = int(f.read().decode('utf-8').strip())
+    except urllib.error.URLError as e:
+        print(e.reason)
+    assert version != 1, 'the version should be higher than 1'
+    this_script_dir = os.path.dirname(sys.argv[0])
+
+    yml = os.path.abspath(f'{this_script_dir}/../.github/workflows/deploy.yml')
+    contents: str = open(yml, 'r').read()
+
+    matches = []
+
+    for result in re.findall(r'bootiful-podcast-action@v.*?\w', contents):
+        print('----')
+        print(result)
+        print(type(result))
+        matches.append(result)
+
+    assert len(matches) == 1, 'there should only be one match!'
+
+    new_content = contents.replace(matches[0], 'bootiful-podcast-action@v%s' % version)
+    with open(yml, 'w') as fp:
+        fp.write(new_content)
         # fp.write ( int(version) * ' ')
-
-
--- bin/run.sh
+++ bin/run.sh
@@ -6,17 +6,16 @@ echo "Loaded repository utils"
 
 function deploy_system_app() {
 
-
   ## fetch the latest version and rewrite the build file
-  VERSION=$(curl https://raw.githubusercontent.com/bootiful-podcast/bootiful-podcast-action/main/version )
-  $(dirname $0)/replace_version.py $VERSION
+  ## VERSION=$(curl https://raw.githubusercontent.com/bootiful-podcast/bootiful-podcast-action/main/version )
+
+  $(dirname $0)/replace_version.py
   git commit -am updating\ version\ to\ $VERSION && git push || echo "nothing to commit and push "
 
   APP_NAME=bootiful-podcast-action-test
   echo "Deploying $APP_NAME to environment $BP_MODE_LOWERCASE "
 
-  if [ -z "${GH_PERSONAL_ACCESS_TOKEN}" ]
-  then
+  if [ -z "${GH_PERSONAL_ACCESS_TOKEN}" ]; then
     echo "The Github personal access token is empty!"
     exit 1
   fi
@@ -25,12 +24,12 @@ function deploy_system_app() {
 
   PAYLOAD='{"event_type":"deploy-event"}'
 
-#  if [ "$BP_MODE_LOWERCASE" = "production" ]; then
-#    PAYLOAD='{"event_type":"deploy-production-event"}'
-#  fi
+  #  if [ "$BP_MODE_LOWERCASE" = "production" ]; then
+  #    PAYLOAD='{"event_type":"deploy-production-event"}'
+  #  fi
 
   echo $(curl -X POST -H "Accept: application/vnd.github.v3+json" \
-      -H "Authorization: token ${GH_PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/bootiful-podcast/${APP_NAME}/dispatches -d $PAYLOAD )
+    -H "Authorization: token ${GH_PERSONAL_ACCESS_TOKEN}" https://api.github.com/repos/bootiful-podcast/${APP_NAME}/dispatches -d $PAYLOAD)
 }
 
 deploy_system_app
